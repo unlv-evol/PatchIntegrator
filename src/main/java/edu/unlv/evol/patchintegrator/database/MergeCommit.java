cf3e4d6a97f97e8ee8b91ed25d689d3e7558d9a5
danielogen <danielogen@gmail.com>
2023-03-20 18:16:16 +0300

    ft: manage all merge commits

---
+++ src/main/java/edu/unlv/evol/patchintegrator/database/MergeCommit.java
@@ -0,0 +1,47 @@
+package edu.unlv.evol.patchintegrator.database;
+
+import org.javalite.activejdbc.Model;
+import org.javalite.activejdbc.annotations.Table;
+
+@Table("merge_commit")
+public class MergeCommit extends Model {
+
+    public MergeCommit(){}
+
+    public MergeCommit(String commitHash, boolean isConflicting, String parent1, String parent2, Project project,
+                       String authorName, String authorEmail, int timestamp) {
+        set("commit_hash", commitHash, "project_id", project.getId(), "is_conflicting", isConflicting, "parent_1", parent1,
+                "parent_2", parent2, "is_done", false, "author_name", authorName, "author_email", authorEmail,
+                "timestamp", timestamp);
+    }
+
+    public int getProjectId() {
+        return getInteger("project_id");
+    }
+
+    public String getCommitHash() {
+        return getString("commit_hash");
+    }
+
+    public String getParent1() {
+        return getString("parent_1");
+    }
+
+
+    public String getParent2() {
+        return getString("parent_2");
+    }
+
+    public boolean isDone() {
+        return getBoolean("is_done");
+    }
+
+    public void setDone() {
+        setBoolean("is_done", true);
+    }
+
+    public void setCommitDetails(String authorName, String authorEmail, int timestamp) {
+        set("author_name", authorName, "author_email", authorEmail,
+                "timestamp", timestamp);
+    }
+}
