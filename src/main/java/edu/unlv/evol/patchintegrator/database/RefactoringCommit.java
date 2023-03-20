8529266cd172b764110ba8597256d383c79902b5
danielogen <danielogen@gmail.com>
2023-03-20 18:15:57 +0300

    ft: all commit changes due to refactoring

---
+++ src/main/java/edu/unlv/evol/patchintegrator/database/RefactoringCommit.java
@@ -0,0 +1,40 @@
+package edu.unlv.evol.patchintegrator.database;
+
+import org.javalite.activejdbc.Model;
+import org.javalite.activejdbc.annotations.Table;
+
+@Table("refactoring_commit")
+public class RefactoringCommit extends Model {
+
+    public RefactoringCommit() {
+    }
+
+    public RefactoringCommit(String commitHash, Object projectId) {
+        set("commit_hash", commitHash, "project_id", projectId, "is_done", 0);
+    }
+
+    public int getID() {
+        return getInteger("id");
+    }
+
+    public int getProjectId() {
+        return getInteger("project_id");
+    }
+
+    public String getCommitHash() {
+        return getString("commit_hash");
+    }
+
+    public boolean isProcessed() {
+        return getBoolean("is_done");
+    }
+
+    public void setDone() {
+        setInteger("is_done", 1);
+    }
+
+    public void setTimedOut() {
+        setInteger("is_done", 2);
+    }
+
+}
