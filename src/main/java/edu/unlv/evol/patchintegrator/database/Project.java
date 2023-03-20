3620464a262959129e80f953046e2f2d09625389
danielogen <danielogen@gmail.com>
2023-03-20 18:14:43 +0300

    ft: setup project class

---
+++ src/main/java/edu/unlv/evol/patchintegrator/database/Project.java
@@ -0,0 +1,36 @@
+package edu.unlv.evol.patchintegrator.database;
+
+import org.javalite.activejdbc.Model;
+import org.javalite.activejdbc.annotations.Table;
+
+@Table("project")
+public class Project extends Model {
+
+    static {
+        validatePresenceOf("url", "name");
+    }
+
+    public Project() {
+    }
+
+    public Project(String url, String name) {
+        set("url", url, "name", name, "is_done", false);
+    }
+
+    public String getName() {
+        return getString("name");
+    }
+
+    public String getURL(){
+        return getString("url");
+    }
+
+    public boolean isDone() {
+        return getBoolean("is_done");
+    }
+
+    public void setDone() {
+        setBoolean("is_done", true);
+    }
+
+}
