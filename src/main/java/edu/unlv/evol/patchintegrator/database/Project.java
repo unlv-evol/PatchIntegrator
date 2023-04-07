dfc2d091168d9e6a412513c64d4cdd139ae6baa3
danielogen <danielogen@gmail.com>
2023-04-07 06:12:58 +0300

    feat: rename fields and add new ones

--- src/main/java/edu/unlv/evol/patchintegrator/database/Project.java
+++ src/main/java/edu/unlv/evol/patchintegrator/database/Project.java
@@ -7,22 +7,37 @@ import org.javalite.activejdbc.annotations.Table;
 public class Project extends Model {
 
     static {
-        validatePresenceOf("url", "name");
+        validatePresenceOf("fork_url", "fork_name");
     }
 
     public Project() {
     }
 
-    public Project(String url, String name) {
-        set("url", url, "name", name, "is_done", false);
+    public Project(String source_url, String fork_url, String source_name, String fork_name) {
+        set("source_url", source_url,
+                "fork_url", fork_url,
+                "source_name", source_name,
+                "fork_name", fork_name,
+                "is_done", false
+        );
     }
 
     public String getName() {
-        return getString("name");
+        return getString("fork_name");
+    }
+    public String getSourceName() {
+        return getString("source_name");
+    }
+    public String getForkName() {
+        return getString("fork_name");
     }
 
-    public String getURL(){
-        return getString("url");
+    public String getSourceURL() {
+        return getString("source_url");
+    }
+
+    public String getForkURL() {
+        return getString("fork_url");
     }
 
     public boolean isDone() {
